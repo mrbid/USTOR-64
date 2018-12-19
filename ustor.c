add90587a9b36c06a618cab864457c891810d242
James William Fletcher <james@voxdsp.com>
2018-12-19 10:51:26 +0000

    Update ustor.c

--- ustor.c
+++ ustor.c
@@ -26,7 +26,7 @@
     This version of USTOR uses the UDP protocol for maximum QPS, a hash map
     using a prime number, a simple bucketing system that is adequate enough
     for the purpose of dealing with hash collisions in the application of
-    unique filtering and as usual cache efficient pre-allocated memory.
+    unique filtering and, as usual, cache efficient pre-allocated memory.
 
     POSIX Threads are used to thread the Read operations, on a separate
     port with port sharing enabled. (port 7811)
@@ -39,8 +39,8 @@
 
     By default 433,033,301 (433 Million) impressions can be recorded
     not including collisions, which from studying the code you will find
-    are range blocked using two short integers to make the whole struct
-    a total size of 8 bytes.
+    collisions are range blocked using two short integers to make the
+    whole struct a total size of 8 bytes.
 
     The current configuration uses ~3.2 GB of memory.
 
